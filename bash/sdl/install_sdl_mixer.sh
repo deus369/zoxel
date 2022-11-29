c1e022eecc664ffa8acedd051cc5461011819ee6
deus <deus3.deus6.deus9@gmail.com>
2022-11-29 18:56:53 +1100

    Added sdl mixer install file

---
+++ bash/sdl/install_sdl_mixer.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+echo Installing latest sdl mixer library
+
+echo Removing any leftover files
+rm master.zip
+rm -r SDL_mixer-main
+
+wget github.com/libsdl-org/SDL_mixer/archive/master.zip
+
+echo unzipping
+unzip master.zip
+rm master.zip
+
+echo building sdl mixer
+cd SDL_mixer-main
+sh autogen.sh
+./configure
+make
+make install
+
+if [[ $LD_LIBRARY_PATH =! /usr/local/lib ]]; then
+	echo Path [/usr/local/lib] already exists in LD_LIBRARY_PATH
+else
+	echo Adding Path [/usr/local/lib] to LD_LIBRARY_PATH
+	export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+fi
+
+echo Removing install folder SDL_mixer-main
+rm -r SDL_mixer-main
+
+echo Finished installing latest sdl mixer library.
+
+sleep 20
