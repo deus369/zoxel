313fca1f6a1f06efb6cba77976f8c92e90e09c0c
deus <deus3.deus6.deus9@gmail.com>
2022-12-27 17:48:50 +1100

    Updated git pull with ssh add function for github pulling

--- bash/git/git_pull.sh
+++ bash/git/git_pull.sh
@@ -1,5 +1,14 @@
 #! /bin/bash
 
-cd ~/zoxel
+cd ../../
+
+ssh_key_location=$HOME/.ssh/zoxel
+echo "Adding ssh key for github [$ssh_key_location]."
+ssh-add $ssh_key_location
+ssh -T git@github.com
 
 git pull
+
+echo "Finished git pull."
+
+sleep 10
