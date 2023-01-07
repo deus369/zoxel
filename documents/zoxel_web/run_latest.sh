72033c1ad2a15e3ef3e4eabdcfd3b12807a99fd2
deus <deus3.deus6.deus9@gmail.com>
2023-01-08 01:00:13 +1100

    Removed zoxel_web build. Updated index.html. Added web todo list inside zoxel_web c file.

--- documents/zoxel_web/run_latest.sh
+++ documents/zoxel_web/run_latest.sh
@@ -3,6 +3,6 @@
 
 sudo cc zoxel_web.c -o zoxel_web && sudo ./zoxel_web
 
-echo "Finished running server."
+# echo "Finished running server."
 
-sleep 30
+# sleep 30
--- documents/zoxel_web/zoxel_web.c
+++ documents/zoxel_web/zoxel_web.c
@@ -8,6 +8,19 @@
 #include "zoxel_sockets.c"
 // #include <vte/vte.h> // for VteTerminal
 
+// todo:
+//      Check if local IP changes
+//        Can I update that on router automatically? port forwarding, maybe new router?
+//      Can I run a test in terminal to see if website url works, and if it doesn't notify myself
+//        Linux notifications, can I use those?
+//      Center the tab buttons?
+//      Header image?
+//      Find a minify html tool that works in terminal
+//      Fix lists
+//      Just go over Zoxel content again
+//      A Roadmap tab?
+//      Set dropdown item to active like tab buttons
+
 // variables
 int running = 1;
 int exit_reason = 0;
