57d0a309d8eb5e529bf33f1be6e803be7a4478fe
deus <deus3.deus6.deus9@gmail.com>
2023-03-15 19:07:50 +1100

    Testing on windows 6

--- src/core/apps/util/sdl_util.c
+++ src/core/apps/util/sdl_util.c
@@ -3,11 +3,11 @@ const char *iconFilename = resources_folder_name"textures/game_icon.png";
 int2 screen_dimensions = { 720, 480 };
 float aspectRatio = 1;
 float fov = 60;
-unsigned long windowFlags;
+// unsigned long window_flags;
+// SDL_WINDOW_SHOWN | SDL_WINDOW_FULLSCREEN
 
 //! Print debug info!
-void print_sdl()
-{
+void print_sdl() {
     #ifdef zoxel_debug_sdl
         zoxel_log(" > sdl stats\n");
         zoxel_log("     + platform:     %s\n", SDL_GetPlatform());
@@ -22,23 +22,20 @@ void print_sdl()
     #endif
 }
 
-int2 get_sdl_screen_size()
-{
+int2 get_sdl_screen_size() {
     int2 screen_size = int2_zero;
     SDL_DisplayMode displayMode;
     SDL_GetCurrentDisplayMode(0, &displayMode);
     screen_size.x = displayMode.w;
     screen_size.y = displayMode.h;
-    if (halfscreen)
-    {
+    if (halfscreen) {
         screen_size.x /= 2;
         screen_size.y /= 2;
     }
     return screen_size;
 }
 
-void set_screen_size()
-{
+void set_screen_size() {
     #ifndef WEB_BUILD
         screen_dimensions = get_sdl_screen_size();
     #else
@@ -46,8 +43,7 @@ void set_screen_size()
     #endif
 }
 
-void sdl_toggle_fullscreen(SDL_Window* window)
-{
+void sdl_toggle_fullscreen(SDL_Window* window) {
     #ifndef ANDROID_BUILD
         Uint32 fullscreen_flag = SDL_WINDOW_FULLSCREEN_DESKTOP; // SDL_WINDOW_FULLSCREEN;
     #else
@@ -73,10 +69,8 @@ int opengl_es_supported() {
 }
 
 //! Initialize SDL things, thingy things.
-int set_sdl_attributes(unsigned char vsync)
-{
-    if (SDL_Init(SDL_INIT_VIDEO) != 0)
-    {
+int set_sdl_attributes(unsigned char vsync) {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         // zoxel_log(stderr, "Failed to Initialize SDL2: %s\n", SDL_GetError());
         zoxel_log("Failed to Initialize SDL2: %s\n", SDL_GetError());
         return EXIT_FAILURE;
@@ -104,8 +98,7 @@ int set_sdl_attributes(unsigned char vsync)
 }
 
 //! Loads a Game Icon from the ui-icons folder.
-void load_app_icon(SDL_Window* window)
-{
+void load_app_icon(SDL_Window* window) {
 #ifdef SDL_IMAGES
     char* fullpath = get_full_file_path(iconFilename);
     SDL_Surface *surface = IMG_Load(iconFilename); // IMG_Load(buffer);
@@ -118,62 +111,50 @@ void load_app_icon(SDL_Window* window)
 }
 
 //! Spawn the SDLWindow.
-SDL_Window* spawn_sdl_window()
-{
+SDL_Window* spawn_sdl_window() {
     set_screen_size();
     unsigned char is_resizeable = 1;
-    windowFlags = SDL_WINDOW_OPENGL;
+    unsigned long window_flags = SDL_WINDOW_OPENGL;
     #ifdef ANDROID_BUILD
-        windowFlags = SDL_WINDOW_FULLSCREEN_DESKTOP; // SDL_WINDOW_FULLSCREEN;
+        window_flags = SDL_WINDOW_FULLSCREEN_DESKTOP;
         is_resizeable = false;
     #endif
-    int2 app_position = (int2) { };
-    int displays = SDL_GetNumVideoDisplays();
-    if (displays > 1 && window_index != -1 && window_index < displays)
-    {
+    int2 app_position = (int2) { 0, 0 };
+    /*int displays = SDL_GetNumVideoDisplays();
+    if (displays > 1 && window_index != -1 && window_index < displays) {
         app_position.x = screen_dimensions.x * window_index;
-    }
-    SDL_Window* window = SDL_CreateWindow("Zoxel",
-        app_position.x, app_position.y,
-        screen_dimensions.x, screen_dimensions.y, // windowFlags);
-        SDL_WINDOW_OPENGL); //  | SDL_WINDOW_SHOWN);
-    if (window == NULL)
-    {
-        SDL_Quit();
-        // fprintf(stderr, "Failed to Create SDLWindow: %s\n", SDL_GetError());
-        zoxel_log(" - failed to create sdl window: %s\n", SDL_GetError());
+    }*/
+    SDL_Window* window = SDL_CreateWindow("Zoxel", app_position.x, app_position.y,
+        screen_dimensions.x, screen_dimensions.y, window_flags);
+    if (window == NULL) {
+        zoxel_log(" - failed to create sdl window [%s]\n", SDL_GetError());
         return window;
     }
     int didFail = set_sdl_attributes(vsync);
     print_sdl();
-    if (didFail == EXIT_FAILURE)
-    {
+    if (didFail == EXIT_FAILURE) {
         zoxel_log("Failed to set_sdl_attributes.");
         return NULL;
     }
-    SDL_GL_SetSwapInterval(1);
     load_app_icon(window);
-    SDL_SetWindowResizable(window, is_resizeable); // SDL_TRUE);
+    SDL_GL_SetSwapInterval(1);
+    SDL_SetWindowResizable(window, is_resizeable);
     SDL_GL_SwapWindow(window);
     return window;
 }
 
-SDL_GLContext* create_sdl_context(SDL_Window* window)
-{
-    if (window == NULL)
-    {
+SDL_GLContext* create_sdl_context(SDL_Window* window) {
+    if (window == NULL) {
         return NULL;
     }
     SDL_GLContext* context = SDL_GL_CreateContext(window);
-    if (context == NULL)
-    {
+    if (context == NULL) {
         zoxel_log(" - failed to create opengl context [%s]\n", SDL_GetError());
         zoxel_log(" > falling back to opengl core profile\n");
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
         context = SDL_GL_CreateContext(window);
     }
-    if (context == NULL)
-    {
+    if (context == NULL) {
         // zoxel_log(stderr, "Failed to Create OpenGL Context: %s\n", SDL_GetError());
         zoxel_log(" - failed again to create opengl context [%s]\n", SDL_GetError());
     }
@@ -185,15 +166,13 @@ extern void input_extract_from_sdl_per_frame(ecs_world_t *world);
 extern void resize_cameras(int width, int height);
 extern void uis_on_viewport_resized(ecs_world_t *world, int width, int height);
 
-void on_viewport_resized(ecs_world_t *world, int width, int height)
-{
+void on_viewport_resized(ecs_world_t *world, int width, int height) {
     #ifdef debug_viewport_resize
     zoxel_log("Viewport was resized [%ix%i]\n", width, height);
     #endif
     screen_dimensions.x = width;
     screen_dimensions.y = height;
-    if(screen_dimensions.y <= 0)
-    {
+    if(screen_dimensions.y <= 0) {
         screen_dimensions.y = 1;
     }
     aspectRatio = ((float) screen_dimensions.x) / ((float) screen_dimensions.y);
@@ -202,43 +181,26 @@ void on_viewport_resized(ecs_world_t *world, int width, int height)
 }
 
 //! Polls SDL for input events. Also handles resize and window exit events.
-void update_sdl(ecs_world_t *world)
-{
+void update_sdl(ecs_world_t *world) {
     input_extract_from_sdl_per_frame(world);
     SDL_Event event = { 0 };
-    while (SDL_PollEvent(&event))
-    {
+    while (SDL_PollEvent(&event)) {
         input_extract_from_sdl(world, event);
         int eventType = event.type;
         // handles application close button
-        if (eventType == SDL_QUIT)
-        {
+        if (eventType == SDL_QUIT) {
             exit_game();
-        }
-        // redo this
-        else if (eventType == SDL_KEYUP)
-        {
-            SDL_Keycode key = event.key.keysym.sym;
-            if (key == SDLK_ESCAPE) 
-            {
-                exit_game();
+        } else if (eventType == SDL_WINDOWEVENT) {
+            if(event.window.event == SDL_WINDOWEVENT_RESIZED || event.window.event == SDL_WINDOWEVENT_SIZE_CHANGED) {
+                on_viewport_resized(world, event.window.data1, event.window.data2);
             }
         }
-        else if (eventType == SDL_WINDOWEVENT)
-        {
-            if(event.window.event == SDL_WINDOWEVENT_RESIZED || event.window.event == SDL_WINDOWEVENT_SIZE_CHANGED)
-            {
-                on_viewport_resized(world, event.window.data1, event.window.data2);
+        // todo: move this to engine code
+        else if (eventType == SDL_KEYUP) {
+            SDL_Keycode key = event.key.keysym.sym;
+            if (key == SDLK_ESCAPE) {
+                exit_game();
             }
         }
     }
 }
-
-/*#ifndef WEB_BUILD
-if (fullscreen) 
-{
-    printf("Setting fullscreen!\n");
-    windowFlags |= SDL_WINDOW_FULLSCREEN_DESKTOP;
-}
-#endif*/
-// SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
