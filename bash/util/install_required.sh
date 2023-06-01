8b6fc4345ca4d353409826c7a41c7ae298a02e74
deus <deus3.deus6.deus9@gmail.com>
2023-06-01 22:36:03 +1000

    fixed updater location

--- bash/util/install_required.sh
+++ bash/util/install_required.sh
@@ -9,21 +9,29 @@ if ! has_library "gcc"; then
 fi
 
 if is_steam_deck; then
+    # for some reason had to manually install gcc
+    # for linux/assert.h
     if ! has_library "glibc"; then
         install_library "glibc"
     fi
-
-    if ! has_library "lib32-glibc"; then
-        install_library "lib32-glibc"
-    fi
-
-    if ! has_library "linux-headers"; then
-        install_library "linux-headers"
-    fi
-
+    # for linux/errno.h
     if ! has_library "linux-api-headers"; then
         install_library "linux-api-headers"
     fi
+    # this kinda needs updating? reinstalling?
+    if ! has_library "sdl2"; then
+        install_library "sdl2"
+    fi
+    
+    # if ! has_library "lib32-glibc"; then
+    #     install_library "lib32-glibc"
+    # fi
+    # if ! has_library "linux-headers"; then
+    #     install_library "linux-headers"
+    # fi
+    # if ! has_library "base-devel"; then
+    #     install_library "base-devel"
+    # fi
 fi
 
 # sudo apt install gcc
