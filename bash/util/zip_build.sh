b559769f3fde894389b0abcfcf07347286d23750
deus <deus3.deus6.deus9@gmail.com>
2024-11-15 22:05:13 +1100

    fixed packaging 5

--- bash/util/zip_build.sh
+++ bash/util/zip_build.sh
@@ -5,14 +5,15 @@ os_type=$(uname | cut -c1-5 | tr '[:upper:]' '[:lower:]')
 if [ $os_type == "mingw" ]; then
     os_type="windows"
 fi
-zip_name="build/zoxel_${os_type}_${architecture}.zip"
+zip_name="zoxel_${os_type}_${architecture}.zip"
+zip_path="build/$zip_name"
 #cd build
-if [ -f $zip_name ]; then
-    echo "  - removing old zip file [$zip_name]"
-    rm -f $zip_name
+if [ -f $zip_path ]; then
+    echo "  - removing old zip file [$zip_path]"
+    rm -f $zip_path
     sleep 1
 else
-    echo " + first time building zip [$zip_name]"
+    echo " + first time building zip [$zip_path]"
 fi
 target_executable="$1"
 build_directory="$(dirname $target_executable)"
@@ -20,40 +21,20 @@ target_filename="$(file $target_executable)"
 echo " > using target_executable [$target_executable]"
 echo " > using build_directory: [$build_directory]"
 echo " > using target_filename [$target_filename]"
-echo "  > creating [$zip_name] in build directory"
+echo " + creating zip [$zip_path]"
 
 if [ -d $build_directory ]; then
     echo " + zipping [$build_directory]"
-    zip -j "$zip_name" $build_directory/*
-    zip -r "$zip_name" resources
+    zip -j "$zip_path" $build_directory/*
+    zip -r "$zip_path" resources
 else
     echo " ! [$build_directory] not found"
 fi
 
 if [ -d ../zoxel-web ]; then
     echo " + found zoxel-web directory"
-    echo " + copied zip into it"
-    cp -f $zip_name ../zoxel-web/$zip_name
+    echo " + copied [$zip_path] into [../zoxel-web/$zip_name]"
+    cp -f $zip_path ../zoxel-web/$zip_name
 else
     echo " ! failed finding ../zoxel-web"
 fi
-
-#if [ -f $target_executable ]; then
-#    echo " + [$target_executable] zipping"
-#    # cd $build_directory && zip -r "$zip_name" $target_filename
-#    zip -j "$zip_name" $target_executable
-#else
-#    echo " ! [$target_executable] not found"
-#fi
-#if [ -d $build_directory/resources ]; then
-#    echo " + [$build_directory/resources] zipping"
-#    cd $build_directory && zip -r "$zip_name" resources
-#else
-#    echo " ! [$build_directory/resources] not found"
-#fi
-#if [ -d $build_directory/lib ]; then
-#    echo " + [$build_directory/lib/*] zipping"
-#    cd $build_directory/lib && zip -r "$zip_name" *
-#else
-#    echo " ! [$build_directory/lib] not found"
-#fi
