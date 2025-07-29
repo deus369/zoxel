e700158506f9bc1fa973f8cc8f24b43b8791a603
deus <deus369@disr.it>
2025-07-29 23:22:28 +1000

    moved windows.h up

--- src/main.c
+++ src/main.c
@@ -10,7 +10,14 @@
 #endif
 
 // pre engine defines
-#include "_.c"
+#include "_.c" // defines our platform things too
+
+#ifdef zoxel_on_windows
+    #define max_args 64
+    #define max_arg_len 256
+    #include <windows.h>
+    #include <shellapi.h>
+#endif
 
 // core includes
 #include <signal.h>     // used for detecting cancel
@@ -302,25 +309,25 @@ int main(int argc, char* argv[]) {
 
 #ifdef zoxel_on_windows
 
-    #define max_args 64
-    #define max_arg_len 256
-    #include <windows.h>
-    #include <shellapi.h>
-
-    int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-        int argc = 0;
-        wchar_t** argvW = CommandLineToArgvW(GetCommandLineW(), &argc);
-        // Array to hold arguments
-        static char argv_storage[max_args][max_arg_len];
-        static char* argv[max_args];
-        for (int i = 0; i < argc && i < max_args; i++) {
-            // Convert wide char string to char string
-            wcstombs(argv_storage[i], argvW[i], max_arg_len);
-            argv_storage[i][max_arg_len - 1] = '\0'; // Ensure null termination
-            argv[i] = argv_storage[i];
-        }
-        LocalFree(argvW); // Free memory allocated by CommandLineToArgvW
-        return main(argc, argv);
+int WINAPI WinMain(
+    HINSTANCE hInstance,
+    HINSTANCE hPrevInstance,
+    LPSTR lpCmdLine,
+    int nCmdShow)
+{
+    int argc = 0;
+    wchar_t** argvW = CommandLineToArgvW(GetCommandLineW(), &argc);
+    // Array to hold arguments
+    static char argv_storage[max_args][max_arg_len];
+    static char* argv[max_args];
+    for (int i = 0; i < argc && i < max_args; i++) {
+        // Convert wide char string to char string
+        wcstombs(argv_storage[i], argvW[i], max_arg_len);
+        argv_storage[i][max_arg_len - 1] = '\0'; // Ensure null termination
+        argv[i] = argv_storage[i];
     }
+    LocalFree(argvW); // Free memory allocated by CommandLineToArgvW
+    return main(argc, argv);
+}
 
 #endif
