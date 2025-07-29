3c4638e8c6d5f54b699b9412bdb2e32c871cc881
deus <deus369@disr.it>
2025-07-29 17:54:00 +1000

    increased ubo size to max allowed per device

--- src/opengl/util/ubo.c
+++ src/opengl/util/ubo.c
@@ -1,14 +1,10 @@
-int zox_get_max_ubo_size() {
-    GLint max_ubo_bytes = 0;
-    glGetIntegerv(GL_MAX_UNIFORM_BLOCK_SIZE, &max_ubo_bytes);
-    // mat4 = 16 floats = 64 bytes
-    return max_ubo_bytes / (sizeof(float) * 16);
-}
-
 int zox_get_safe_ubo_size() {
-#ifdef zox_android
-    return 256; // or query if EGL/OpenGL ES 3.0+ supports glGetIntegerv
-#else
-    return int_min(10000, zox_get_max_ubo_size());
-#endif
+    int size = 0;
+    glGetIntegerv(GL_MAX_UNIFORM_BLOCK_SIZE, &size);
+    // mat4 = 16 floats = 64 bytes
+    size = size / (sizeof(float) * 16);
+    if (size >= 10000) {
+        size = 10000;
+    }
+    return size;
 }
